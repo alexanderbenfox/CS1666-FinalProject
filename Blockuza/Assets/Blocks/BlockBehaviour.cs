080c54abbc691f02b041a726d667b1c9b38938a3
Alex Fox <alexanderbenfox@gmail.com>
2017-03-22 20:22:16 -0400

    Added the block behavior back in

---
+++ Blockuza/Assets/Blocks/BlockBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BlockType{
+	Destroyable, Sliding
+};
+
+public class BlockBehaviour : MonoBehaviour {
+
+	protected SnapToGrid snap;
+	[SerializeField]
+	protected BlockType type;
+	protected PhysicsObject physics;
+
+	void Awake(){
+		type = BlockType.Destroyable;
+		snap = this.GetComponent<SnapToGrid> ();
+	}
+
+	void FixedUpdate () {
+		if(snap.snapped)
+			snap.enabled = false;
+	}
+
+	public BlockType getType(){
+		return type;
+	}
+}

bf7830befca1a4111ff70c3bf2800fb2115e716a
zblouse <zachblouse@gmail.com>
2017-03-24 15:24:46 -0400

    Add files via upload
    The chasm puzzle, as well as the generic puzzle trigger prefab

---
+++ Puzzles/Puzzle.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puzzle : MonoBehaviour {
+	public bool Triggered;
+	// Use this for initialization
+
+}

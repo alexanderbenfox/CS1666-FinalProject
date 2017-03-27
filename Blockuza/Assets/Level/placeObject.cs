d8028e7782d658d8a60cf1d8501ca5d248d52bf5
cam292 <cam292@pitt.edu>
2017-03-27 16:50:04 -0400

    Added place object script to testSection

---
+++ Blockuza/Assets/Level/placeObject.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class placeObject : MonoBehaviour {
+	public double[] objLocations;//see getCoords(double coordinate) for description 
+	public Boolean[] objAbove;
+
+	// Finds index to place the object, then calls validIndex() to place it
+	public void Place (GameObject obj) 
+	{
+		System.Random rand = new System.Random();
+		int index = rand.Next(0, objLocations.Length);
+
+		while (index < objLocations.Length)
+		{
+			if (objAbove[index] == false) //nothing above this square, so place the object
+			{
+				validIndex(index, obj);
+				break;
+			}
+			else //try next index
+			{
+				if (index++ >= objLocations.Length) //restart from beginning if at last index
+				{
+					index = 0;
+				}
+			}
+
+		}
+			
+	}
+
+	// Instantiates the object above coords of index found in Place()
+	private void validIndex(int index, GameObject obj)
+	{
+		objAbove[index] = true;
+		int[] coords = getCoords(objLocations[index]);
+		Instantiate(obj, new Vector2(coords[0], coords[1]+1), Quaternion.identity);
+	}
+
+	/*
+	 * x coordinate is value left of decimal
+	 * y coordinate is value right of decimal
+	 * @return coords[0] is x value, coords[1] is y value
+	 */
+	private int[] getCoords(double coordinate)
+	{
+		int[] coords = new int[2];
+		coords[0] = (int)coordinate; // x value
+		coords[1] = (int)((coordinate - coords[0]) * 10); // y value
+		return coords;
+	}
+
+
+
+}
--- Blockuza/Assets/Level/roomGeneration.cs
+++ Blockuza/Assets/Level/roomGeneration.cs
@@ -5,34 +5,37 @@ using System;
 
 public class roomGeneration : MonoBehaviour {
 
-	public GameObject[] sections;
+	public GameObject[] sectionPrefabs;
+	//public GameObject[] puzzlePrefabs;
+	public GameObject mainCharPrefab;
+	//public int enemyConcentration; // num enemies per section
 	public int sectionsWide;
-	public int sectionsHigh;
+	//public int numPuzzles; //number of puzzles in the room
+	//public int level; // will determine if left to right, or right to left
 
 	private float cellSize = 1.6f; // 5 blocks x .32 wide
+	//private GameObject[] 
 
 	// Use this for initialization
 	void Start () 
 	{
-		Debug.Log(sections.Length);
-		Debug.Log(sections[0]);
-		Generate();
+		//int numSections = sectionsWide / numPuzzles; //number of 
+		GenerateSection();
 	}
 	
 	// Update is called once per frame
-	void Generate() 
+	void GenerateSection() 
 	{
-		GameObject currentSection = sections[0];
+		GameObject currentSection = sectionPrefabs[0];
 		System.Random rand = new System.Random(2);
 
 		for (int i = 0; i < sectionsWide; i++)
 		{
-			for (int j = 0; j < sectionsHigh; j++)
-			{
-				Instantiate(currentSection, new Vector2(i*cellSize, j*cellSize), Quaternion.identity);
-				int next = rand.Next(0, sections.Length);
-				currentSection = sections[next];
-			}
+			Instantiate(currentSection, new Vector2(i*cellSize, 0), Quaternion.identity); //place section
+			placeObject placeComp = currentSection.GetComponent<placeObject>(); //get placeObject component on section
+			placeComp.Place(mainCharPrefab); //call Place() to put an object in the section
+			int next = rand.Next(0, sectionPrefabs.Length);
+			currentSection = sectionPrefabs[next];
 		}
 	}
 }

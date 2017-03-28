809b260c3aa0a3fb20bef5ca1dabb579ece0d5a5
Mick Lichtenberg <mrl69@pitt.edu>
2017-03-28 16:22:13 -0400

    Add two puzzles in PuzzleTest scene

---
+++ Blockuza/Assets/Level/Puzzles/PuzzleTriggerScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleTriggerScript : MonoBehaviour {
+	public Puzzle thisPuzzle;
+	// Use this for initialization
+	void OnTriggerEnter2D(Collider2D col){
+		thisPuzzle.Triggered = true;
+		Debug.Log ("Triggered");
+	}
+	void OnTriggerExit2D(Collider2D col){
+		thisPuzzle.Triggered = false;
+		Debug.Log ("No  Longer Triggered");
+	}
+}

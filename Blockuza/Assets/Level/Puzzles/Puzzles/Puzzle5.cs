2119826c9c20e1d5580a4c6e58212716ec729667
Mick Lichtenberg <mrl69@pitt.edu>
2017-03-30 18:57:39 -0400

    Add Puzzle #5

---
+++ Blockuza/Assets/Level/Puzzles/Puzzles/Puzzle5.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puzzle5 : Puzzle 
+{
+	public GameObject[] TriggerBlocks;
+
+	// Use this for initialization
+	void Start () 
+	{
+		TriggerBlocks[0].SetActive(true);
+		TriggerBlocks[1].SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Triggered)
+		{
+			TriggerBlocks[0].SetActive(false);
+			TriggerBlocks[1].SetActive(true);
+		}
+	}
+}

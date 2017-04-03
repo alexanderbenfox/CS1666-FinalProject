fc8fdc5de1671c6a23d96049b89fd9bf0b070ed7
Alex Fox <alexanderbenfox@gmail.com>
2017-04-03 15:29:56 -0400

    added start scene.  you might have to adjust your build settings to include this and the room test scene to get it to work

---
+++ Blockuza/Assets/StartMenu/StartMenuManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class StartMenuManager : MonoBehaviour {
+
+	public Canvas PlayScreen, HowToPlay, Controls;
+
+	public void GoToHowToPlay(){
+		PlayScreen.gameObject.SetActive (false);
+		HowToPlay.gameObject.SetActive (true);
+		Controls.gameObject.SetActive (false);
+	}
+
+	public void GoToPlayScreen(){
+		PlayScreen.gameObject.SetActive (true);
+		HowToPlay.gameObject.SetActive (false);
+		Controls.gameObject.SetActive (false);
+	}
+
+	public void GoToControlsScreen(){
+		PlayScreen.gameObject.SetActive (false);
+		HowToPlay.gameObject.SetActive (false);
+		Controls.gameObject.SetActive (true);
+	}
+
+}

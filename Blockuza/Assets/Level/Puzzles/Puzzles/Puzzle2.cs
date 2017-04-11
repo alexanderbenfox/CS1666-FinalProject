8e7943131a1b763ea5a7be5f8e6014f4d530804d
Mick Lichtenberg <mrl69@pitt.edu>
2017-04-10 20:11:35 -0400

    Modified old Puzzles and added Puzzle #7

---
+++ Blockuza/Assets/Level/Puzzles/Puzzles/Puzzle2.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puzzle2 : Puzzle {
+
+	public GameObject Enemies;
+
+	// Use this for initialization
+	void Start () 
+	{
+		Enemies.SetActive(false);	
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Triggered)
+		{
+			Enemies.SetActive(true);
+		}
+		else
+		{
+			Enemies.SetActive(false);
+		}
+	}
+}
---
+++ Blockuza/Assets/Level/Puzzles/Puzzles/RightWallTrigger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RightWallTrigger : Puzzle {
+
+	public GameObject RightWall;
+
+	// Use this for initialization
+	void Start () 
+	{
+		RightWall.SetActive(true);	
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Triggered)
+		{
+			RightWall.SetActive(false);
+		}
+		else
+		{
+			RightWall.SetActive(true);
+		}
+	}
+}

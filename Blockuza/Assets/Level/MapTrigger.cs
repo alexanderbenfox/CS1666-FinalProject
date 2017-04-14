3904bc72043aa011ec280eb3d8be49074d0a6199
cam292 <cam292@pitt.edu>
2017-04-14 13:15:23 -0400

    Added UI for when player falls off

--- Blockuza/Assets/Level/MapTrigger.cs
+++ Blockuza/Assets/Level/MapTrigger.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class MapTrigger : MonoBehaviour {
 
-
+	public GameObject UI;
+	public float sec;
+	private GameObject instance;
 
 	void OnTriggerEnter2D(Collider2D col)
 	{
@@ -12,6 +14,16 @@ public class MapTrigger : MonoBehaviour {
 		{
 			Debug.Log("Door hit");
 			col.transform.position = new Vector2(0, 0.64f);
+			instance = Instantiate(UI, new Vector2(0, 0), Quaternion.identity);
+			StartCoroutine(timer());
 		}
+	}
+
+	IEnumerator timer()
+	{
+
+		yield return new WaitForSeconds(sec);
+		Destroy(instance);
+
 	}
 }

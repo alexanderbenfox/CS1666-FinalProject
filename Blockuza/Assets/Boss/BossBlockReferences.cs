5943f648e816611b014129bb2b305e3a9983b40c
zblouse <zachblouse@gmail.com>
2017-04-13 20:08:05 -0400

    Add files via upload

---
+++ Blockuza/Assets/Boss/BossBlockReferences.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossBlockReferences : MonoBehaviour {
+
+	public GameObject boss32;
+	public GameObject boss64;
+	public GameObject boss128;
+	public bool beingCreated=false;
+	private bool waitFrame;
+	void Update(){
+		if (waitFrame) {
+			beingCreated = false;
+			waitFrame = false;
+		}
+		if (beingCreated) {
+			waitFrame = true;
+		}
+	}
+}
--- Blockuza/Assets/Boss/BossBlockTiled.cs
+++ Blockuza/Assets/Boss/BossBlockTiled.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BossBlockTiled : MonoBehaviour {
-	public GameObject boss32Prefab;
-	public GameObject boss64Prefab;
+	public GameObject references;
+	private GameObject block32;
+	private GameObject block64;
+	private GameObject block128;
 	private PhysicsObject physics;
+	public int size;
 	public bool moving;
 	private float waitTime;
 	public bool waitingLeft=true;
@@ -16,16 +19,49 @@ public class BossBlockTiled : MonoBehaviour {
 	public bool waitFrame=false;
 	private float bossFrameTime=.2f;
 	public bool gravity = false;
+	public bool meCreate=true;
 	public int state;//Assuming clockwise motion, 1 is moving right, on top of a block, 2 is moving down the right side of a block, 3 is moving left under a block, and 4 is moving up the left side of a block
 	// Use this for initialization
 	void Start () {
 		state = 1;
+		references = GameObject.Find("BossBlockReferences");
+		block32 = references.GetComponent<BossBlockReferences>().boss32;
+		block64 = references.GetComponent<BossBlockReferences>().boss64;
+		block128 = references.GetComponent<BossBlockReferences>().boss128;
 		physics = gameObject.GetComponent<PhysicsObject> ();
 		snap = gameObject.GetComponent<SnapToGrid> ();
 	}
 	void OnTriggerEnter2D(Collider2D col){
-		if (col.tag == "Boss") {
+		if (col.tag == "Boss32" && size==32) {
 			Debug.Log ("Boss collide");
+			if (meCreate) {
+				Vector3 myPosition = gameObject.transform.position;
+				col.GetComponent<BossBlockTiled> ().meCreate = false;
+				references.GetComponent<BossBlockReferences> ().beingCreated = true;
+				GameObject newBlock = Instantiate (block64, myPosition, Quaternion.identity);
+			}
+			Destroy (gameObject);
+		}
+		if (col.tag == "Boss64" && size==64) {
+			Debug.Log ("Boss collide");
+			if (meCreate) {
+				Vector3 myPosition = gameObject.transform.position;
+				col.GetComponent<BossBlockTiled> ().meCreate = false;
+				references.GetComponent<BossBlockReferences> ().beingCreated = true;
+				GameObject newBlock = Instantiate (block128, myPosition, Quaternion.identity);
+			}
+			Destroy (gameObject);
+		}
+		if (col.tag == "Block" && size==64) {
+			Vector3 myPosition = gameObject.transform.position;
+			GameObject newBlock = Instantiate (block32, myPosition, Quaternion.identity);
+			GameObject newBlock2 = Instantiate (block32, new Vector3(myPosition.x-.64f,myPosition.y,0), Quaternion.identity);
+			Destroy (gameObject);
+		}
+		if (col.tag == "Block" && size==128) {
+			Vector3 myPosition = gameObject.transform.position;
+			GameObject newBlock = Instantiate (block64, myPosition, Quaternion.identity);
+			GameObject newBlock2 = Instantiate (block64, new Vector3(myPosition.x-1.28f,myPosition.y,0), Quaternion.identity);
 			Destroy (gameObject);
 		}
 	}
